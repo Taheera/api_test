0dca0a4d2a62cbe8dbaa6b8194b0b9fed3e9135d
Taheera <42031282+Taheera@users.noreply.github.com>
2018-09-19 11:58:25 +0000

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,7 +1,8 @@
-#from __future__ import print_function
+from __future__ import print_function
 import flask
 from flask import request, jsonify
 import re, math
+import pandas as pd
 import json
 import os
 
@@ -11,14 +12,24 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-	return "<h1>API</h1><p>Test Api</p>"
+    return "<h1>API</h1><p>Test Api</p>"
 
 @app.route('/api', methods=['GET'])
 def addjd():
-	if 'questions' in request.args:
+    if 'questions' in request.args:
 		b = str(request.args['questions'])
-		print("success" + b)
+		print("success" + " " +b)
 		return "success" + " " + b
-	else:
-		return "false"
-app.run()
+    else:
+        return "false"
+
+if __name__ == '__main__':
+    
+    import logging
+    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
